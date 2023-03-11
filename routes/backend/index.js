1b4e96923b4da34553266b64cde1e40f85d40247
Charles <cchester1@angelo.edu>
2023-03-10 18:09:50 -0600

    add utilites folder and import utilities into api routes file

---
+++ routes/backend/index.js
@@ -0,0 +1,7 @@
+router = require('express').Router()
+const data =  require('../../data/data.json')
+// import utilities
+
+router.get('/animals', (req, res) => {
+
+})
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router()
 const frontend = require('./frontend')
+const backend = require('./backend')
 
 router.use('/', frontend)
+router.use('/api', backend)
 
 module.exports = router;
